b1e8d54767a3e098295d4a4b4a59335a156cbd17
HydraLM81 <hydralm81@gmail.com>
2023-06-27 16:56:39 -0700

    start of theme selector

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -11,90 +11,100 @@ listMenu.style.top = topValue;
 
 function handleHover(event) {
   const animeEntry = event.target.closest('tbody.list-item');
+  if(animeEntry.querySelector('.title a')){
+    const titleElement = animeEntry.querySelector('.title a');
+    const airElement = animeEntry.querySelector('.content-status');
+    const descriptionElement = animeEntry.querySelector('.text.notes-52034, .text:not(.notes-52034)');
+    const scoreElement = animeEntry.querySelector('.score-label');
+    const imageElement = animeEntry.querySelector('.image img');
+    const animeLink = animeEntry.querySelector('.data.title.clearfix a');
+    const animeUrl = animeLink.href;
+    const editLink = animeEntry.querySelector('.add-edit-more .edit a');
+    const editUrl = editLink.href;
 
-  const titleElement = animeEntry.querySelector('.title a');
-  const descriptionElement = animeEntry.querySelector('.text.notes-52034, .text:not(.notes-52034)');
-  const scoreElement = animeEntry.querySelector('.score-label');
-  const imageElement = animeEntry.querySelector('.image img');
-  const animeLink = animeEntry.querySelector('.data.title.clearfix a');
-  const animeUrl = animeLink.href;
-  const editLink = animeEntry.querySelector('.add-edit-more .edit a');
-  const editUrl = editLink.href;
+    if (titleElement && titleElement.textContent) {
 
-  if (titleElement && titleElement.textContent) {
+      /* SIDEBAR */
 
-    /* SIDEBAR */
-
-    const title = titleElement.textContent.trim();
-    const description = descriptionElement ? descriptionElement.textContent.trim() : '';
-    const formattedDescription = description.replace(/\n/g, '<br>'); // fixes newlines so they actually show in the sidebar
-    const score = scoreElement ? scoreElement.textContent.trim() : '';
-    const imageUrl = imageElement ? imageElement.src : '';
-
-
-    // Create the sidebar container
-    const sidebar = document.createElement('div');
-    sidebar.id = 'mal-sidebar';
+      const title = titleElement.textContent.trim();
+      const airStatus = airElement.textContent.trim();
+      const description = descriptionElement ? descriptionElement.textContent.trim() : '';
+      const formattedDescription = description.replace(/\n/g, '<br>'); // fixes newlines so they actually show in the sidebar
+      const score = scoreElement ? scoreElement.textContent.trim() : '';
+      const imageUrl = imageElement ? imageElement.src : '';
 
 
-    // Create the sidebar content
-    const sidebarContent = document.createElement('div');
-    sidebarContent.innerHTML = `
-      <a href="${animeUrl}">
-        <img src="${imageUrl}" alt="${title}" class="anime-image">
-        <h2>${title}</h2>
-      </a>
-      <p1>${formattedDescription}</p1><br><br><br>
-      <p2>Score: ${score}/10</p2> <br><br>
-      <a href="${editUrl}" class="edit-link">Edit</a>
-    `;
+      // Create the sidebar container
+      const sidebar = document.createElement('div');
+      sidebar.id = 'mal-sidebar';
 
 
-    const descriptionParagraph = sidebarContent.querySelector('p1'); // Update tag if it's changed above
-    const regex = /(http[s]?:\/\/[^\s)]+)/g;
-    descriptionParagraph.innerHTML = description.replace(regex, '<a href="$1" target="_blank">$1</a>').replace(/<a href="([^"]+)">([^<]+)<\/a>/g, '<a href="$1" target="_blank">$2</a>');
+      // Create the sidebar content
+      const sidebarContent = document.createElement('div');
+      sidebarContent.innerHTML = `
+        <a href="${animeUrl}">
+          <img src="${imageUrl}" alt="${title}" class="anime-image">
+          <h2>${title}</h2>
+          <h1>${airStatus}</h1> <br><br>
+        </a>`;
+
+        if(formattedDescription!=null) {
+          sidebarContent.innerHTML+=`<p1>${formattedDescription}</p1><br><br><br>`;
+        }
+        if(score!=null) {
+          sidebarContent.innerHTML+=`<p2>Score: ${score}/10</p2> <br><br>`;
+        }
+        if(editUrl!=null) {
+          sidebarContent.innerHTML+=`<a href="${editUrl}" class="edit-link">Edit</a>`;
+        }
 
 
-    /* END OF SIDEBAR*/
-    
-
-    // Create the close button element
-    const closeButton = document.createElement('button');
-    closeButton.textContent = 'X';
-    closeButton.style.position = 'absolute';
-    closeButton.style.top = '5px';
-    closeButton.style.right = '5px';
-    closeButton.style.fontSize = '16px';
-    closeButton.style.fontWeight = 'bold';
-    closeButton.style.background = 'none';
-    closeButton.style.border = 'none';
-    closeButton.style.cursor = 'pointer';
-    // Add event listener to the close button
-    closeButton.addEventListener('click', removeSidebar);
+      const descriptionParagraph = sidebarContent.querySelector('p1'); // Update tag if it's changed above
+      const regex = /(http[s]?:\/\/[^\s)]+)/g;
+      descriptionParagraph.innerHTML = description.replace(regex, '<a href="$1" target="_blank">$1</a>').replace(/<a href="([^"]+)">([^<]+)<\/a>/g, '<a href="$1" target="_blank">$2</a>');
 
 
-    console.log('Anime Title:', title);
-    console.log('Description:', description);
-    console.log('Score:', score);
-    console.log('Image URL:', imageUrl);
+      /* END OF SIDEBAR*/
+      
+
+      // Create the close button element
+      const closeButton = document.createElement('button');
+      closeButton.textContent = 'X';
+      closeButton.style.position = 'absolute';
+      closeButton.style.top = '5px';
+      closeButton.style.right = '5px';
+      closeButton.style.fontSize = '16px';
+      closeButton.style.fontWeight = 'bold';
+      closeButton.style.background = 'none';
+      closeButton.style.border = 'none';
+      closeButton.style.cursor = 'pointer';
+      // Add event listener to the close button
+      closeButton.addEventListener('click', removeSidebar);
 
 
-    removeSidebar();
+      console.log('Anime Title:', title);
+      console.log('Description:', description);
+      console.log('Score:', score);
+      console.log('Image URL:', imageUrl);
 
-    sidebar.appendChild(sidebarContent);
-    sidebar.appendChild(closeButton);
 
-    // Insert the sidebar into the body element
-    document.body.appendChild(sidebar);
+      removeSidebar();
 
-    //move side menu to right of sidebar
-    const listMenuFloat = document.querySelector('.list-menu-float');
-    if (listMenuFloat) {
-      const sidebarWidth = sidebar.getBoundingClientRect().width;
-      listMenuFloat.style.left = `${sidebarWidth}px`;
+      sidebar.appendChild(sidebarContent);
+      sidebar.appendChild(closeButton);
+
+      // Insert the sidebar into the body element
+      document.body.appendChild(sidebar);
+
+      //move side menu to right of sidebar
+      const listMenuFloat = document.querySelector('.list-menu-float');
+      if (listMenuFloat) {
+        const sidebarWidth = sidebar.getBoundingClientRect().width;
+        listMenuFloat.style.left = `${sidebarWidth}px`;
+      }
+
+      console.log('Sidebar displayed');
     }
-
-    console.log('Sidebar displayed');
   }
 }
 
@@ -107,3 +117,48 @@ function removeSidebar() {
 }
 
 document.addEventListener('mouseover', handleHover);
+
+
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === 'updateTheme') {
+    const selectedTheme = message.selectedTheme;
+    const themeVariables = getThemeVariables(selectedTheme);
+
+    // Update the CSS variables in sidebar-style.css
+    const root = document.documentElement;
+    Object.keys(themeVariables).forEach((key) => {
+      root.style.setProperty(`--${key}`, themeVariables[key]);
+    });
+  }
+});
+
+function getThemeVariables(selectedTheme) {
+  // Define the CSS variables for each theme
+  const themes = {
+    defaultTheme: {
+      primaryColor: '#000000',
+      secondaryColor: '#ffffff',
+      // Add more CSS variables for the default theme
+    },
+    white: {
+      primaryColor: '#333333',
+      secondaryColor: '#eeeeee',
+      // Add more CSS variables for the dark theme
+    },
+    // Define more themes here
+    // theme1: {
+    //   primaryColor: '#ff0000',
+    //   secondaryColor: '#00ff00',
+    //   // Add more CSS variables for theme1
+    // },
+    // theme2: {
+    //   primaryColor: '#0000ff',
+    //   secondaryColor: '#ffff00',
+    //   // Add more CSS variables for theme2
+    // },
+  };
+
+  // Return the CSS variables for the selected theme
+  return themes[selectedTheme] || themes.default;
+}
--- extension/popupScript.js
+++ extension/popupScript.js
@@ -1,50 +1,35 @@
-// Popup script to receive anime details and handle editing options
-
 // Get DOM elements
-const animeTitleElem = document.getElementById('animeTitle');
-const animeDescriptionElem = document.getElementById('animeDescription');
-const animeScoreElem = document.getElementById('animeScore');
-const editTitleElem = document.getElementById('editTitle');
-const editDescriptionElem = document.getElementById('editDescription');
-const editScoreElem = document.getElementById('editScore');
-const saveButtonElem = document.getElementById('saveButton');
+const themeSelect = document.getElementById('themeSelect');
 
-// Receive anime details from content script
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'animeHovered') {
-    console.log("request.action==animeHovered triggered and passed");
-    animeTitleElem.textContent = request.title;
-    animeDescriptionElem.textContent = request.description;
-    animeScoreElem.textContent = request.score;
-    editTitleElem.value = request.title;
-    editDescriptionElem.value = request.description;
-    editScoreElem.value = request.score;
+// Retrieve the selected theme from local storage
+chrome.storage.local.get('selectedTheme', function(result) {
+  const selectedTheme = result.selectedTheme || 'default';
+  themeSelect.value = selectedTheme;
 
-    // Show the sidebar
-    document.body.classList.add('sidebar-visible');
-  }
+  // Send a message to the content script to update the theme
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const message = {
+      action: 'updateTheme',
+      selectedTheme,
+    };
+    chrome.tabs.sendMessage(tabs[0].id, message);
+  });
 });
 
-// Hide the sidebar when the mouse leaves the popup
-document.addEventListener('mouseleave', function() {
-  document.body.classList.remove('sidebar-visible');
-  console.log("mouseleave event triggered");
-});
+// Theme select change event handler
+themeSelect.addEventListener('change', function(event) {
+  const selectedTheme = event.target.value;
 
-// Save button click event handler
-saveButtonElem.addEventListener('click', function() {
-  console.log("click event triggered");
-  const editedTitle = editTitleElem.value;
-  const editedDescription = editDescriptionElem.value;
-  const editedScore = editScoreElem.value;
+  // Store the selected theme in local storage
+  chrome.storage.local.set({ selectedTheme });
+  console.log('Theme changed to ',selectedTheme);
 
-  // Perform the necessary actions with the edited data, e.g., send it to the server or update the DOM
-
-  // Display a success message
-  saveButtonElem.textContent = 'Saved!';
-  setTimeout(function() {
-    saveButtonElem.textContent = 'Save';
-  }, 1000);
-
-  console.log("click event finished");
+  // Send a message to the content script to update the theme
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const message = {
+      action: 'updateTheme',
+      selectedTheme,
+    };
+    chrome.tabs.sendMessage(tabs[0].id, message);
+  });
 });
