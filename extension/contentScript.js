7a3ae93edc0f1a4c83182669e8bba998295eeed4
HydraLM81 <hydralm81@gmail.com>
2023-06-26 22:01:04 -0700

    Remove null area error

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -2,13 +2,19 @@
 
 // Listener for hover events on anime entries
 document.addEventListener('mouseover', function(event) {
-    const animeEntry = event.target.closest('.list-table-data');
-  
-    if (animeEntry) {
-      const animeTitle = animeEntry.querySelector('.animetitle').textContent.trim();
-      const animeDescription = animeEntry.querySelector('.pt4').textContent.trim();
-      const animeScore = animeEntry.querySelector('.score .score-label').textContent.trim();
-  
+  const animeEntry = event.target.closest('.list-table-data');
+
+  if (animeEntry) {
+    const animeTitleElem = animeEntry.querySelector('.animetitle');
+    const animeDescriptionElem = animeEntry.querySelector('.pt4');
+    const animeScoreElem = animeEntry.querySelector('.score .score-label');
+
+    // Check if the elements are found in the DOM
+    if (animeTitleElem && animeDescriptionElem && animeScoreElem) {
+      const animeTitle = animeTitleElem.textContent.trim();
+      const animeDescription = animeDescriptionElem.textContent.trim();
+      const animeScore = animeScoreElem.textContent.trim();
+
       // Send anime details to the extension popup
       chrome.runtime.sendMessage({
         action: 'animeHovered',
@@ -17,5 +23,5 @@ document.addEventListener('mouseover', function(event) {
         score: animeScore
       });
     }
-  });
-  
+  }
+});
